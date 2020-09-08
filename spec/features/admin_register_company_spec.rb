9ae55000540b3a875a44aa0f3e9af316d17e8e3c
SWillames <SWillames>
2020-09-08 09:01:36 -0300

    Removing translation from the tests.

--- spec/features/admin_register_company_spec.rb
+++ spec/features/admin_register_company_spec.rb
@@ -4,17 +4,16 @@ feature 'Admin register company' do
   scenario 'successufuly' do
     visit root_path
 
-    click_on t('home.link.register.company')
-    expect(page).to have_content t('actions.new')
-    expect(page).to have_content t('activerecord.models.company.one')
-    fill_in t('activerecord.attributes.company.name'), with: "Hirthe-Ritchie"
-    fill_in t('activerecord.attributes.company.corporate_name'), with: "Hirthe-Ritchie S/A"
-    fill_in t('activerecord.attributes.company.cnpj'), with: "18553414000618"
-    fill_in t('activerecord.attributes.company.domain'), with: "@hir-rit.com"
-    fill_in t('activerecord.attributes.company.address'), with: "Av. Bucar Neto, 127"
-    fill_in t('activerecord.attributes.company.social_networks'), with: "@hir-rit"
+    click_on 'Empresas'
+    
+    fill_in 'Nome', with: "Hirthe-Ritchie"
+    fill_in 'Razão Social', with: "Hirthe-Ritchie S/A"
+    fill_in 'CNPJ', with: "18.553.414/0006-18"
+    fill_in 'Endereço', with: "Av. Bucar Neto, 127"
+    fill_in 'Dominio', with: "@hir-rit.com"
+    fill_in 'Redes Sociais', with: "@hir-rit"
 
-    click_on t('helpers.submit.create')
+    click_on 'Enviar'
 
     expect(current_path).to eq company_path(Company.last)
     expect(page).to have_content t('notice.register.title')
@@ -30,8 +29,5 @@ feature 'Admin register company' do
     expect(page).to have_content (Company.last.address)
     expect(page).to have_content t('activerecord.attributes.company.social_networks')+':'
     expect(page).to have_content (Company.last.social_networks)
-
-  end
-
-  
+  end  
 end
